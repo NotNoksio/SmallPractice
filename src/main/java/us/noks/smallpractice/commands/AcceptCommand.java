30eb3485d52ad5abe33ed24f04f2bbbd92e34aa3
Noksio <42706907+NoksBlue@users.noreply.github.com>
2019-08-20 18:25:09 -0400

    Update AcceptCommand.java

--- src/main/java/us/noks/smallpractice/commands/AcceptCommand.java
+++ src/main/java/us/noks/smallpractice/commands/AcceptCommand.java
@@ -15,29 +15,30 @@ public class AcceptCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if (sender instanceof Player) {
-			Player player = (Player) sender;
-			
-			if (args.length != 1) {
-				player.sendMessage(ChatColor.RED + "Usage: /accept <player>");
-				return false;
-			}
-			Player dueler = Bukkit.getPlayer(args[0]);
-			
-			if (dueler == null) {
-				player.sendMessage(Messages.getInstance().PLAYER_NOT_ONLINE);
-				return false;
-			}
-			if (dueler == player) {
-				player.sendMessage(Messages.getInstance().NOT_YOURSELF);
-				return false;
-			}
-			if (PlayerManager.get(dueler.getUniqueId()).getRequestTo() != player.getUniqueId()) {
-				player.sendMessage(Messages.getInstance().NO_REQUEST_FOUND);
-				return false;
-			}
-			RequestManager.getInstance().acceptDuelRequest(player, dueler);
+		if (!(sender instanceof Player)) {
+		        return false;
 		}
+		Player player = (Player) sender;
+			
+		if (args.length != 1) {
+			player.sendMessage(ChatColor.RED + "Usage: /accept <player>");
+			return false;
+		}
+		Player dueler = Bukkit.getPlayer(args[0]);
+			
+		if (dueler == null) {
+			player.sendMessage(Messages.getInstance().PLAYER_NOT_ONLINE);
+			return false;
+		}
+		if (dueler == player) {
+			player.sendMessage(Messages.getInstance().NOT_YOURSELF);
+			return false;
+		}
+		if (PlayerManager.get(dueler.getUniqueId()).getRequestTo() != player.getUniqueId()) {
+			player.sendMessage(Messages.getInstance().NO_REQUEST_FOUND);
+			return false;
+		}
+		RequestManager.getInstance().acceptDuelRequest(player, dueler);
 		return true;
 	}
 }
